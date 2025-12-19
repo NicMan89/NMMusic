56f2feb024bdefbc3c84df9e5bac81f0ac5c3c92
Nicola Manconi <81507238+NicMan89@users.noreply.github.com>
2025-12-19 16:30:46 +0100

    modifica file

--- src/services/firebase-config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyD3sB4GmmbFvVjUKsa6c0TxEItHU7lvE4M",
-  authDomain: "mymusic-pwa.firebaseapp.com",
-  projectId: "mymusic-pwa",
-  storageBucket: "mymusic-pwa.firebasestorage.app",
-  messagingSenderId: "359509559806",
-  appId: "1:359509559806:web:57090127f1eda6e0ce98af"
-};
